a244de5ea25c71146b3e1554fb8bae43fd0888ce
John Otander <johnotander@gmail.com>
2015-02-03 11:27:38 -0700

    Add CSS4 media query option

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict';
 
-module.exports = function mediaQueryTypes() {
-  return [
-    'all',
-    'braille',
-    'embossed',
-    'handheld',
-    'print',
-    'projection',
-    'screen',
-    'speech',
-    'tty',
-    'tv'
+module.exports = function mediaQueryTypes(options) {
+  options = options || {};
+
+  var mediaQueryTypes = [
+    'all', 'braille', 'embossed', 'handheld', 'print', 'projection', 'screen',
+    'speech', 'tty', 'tv'
   ];
+
+  var css4MediaQueryTypes = ['pointer', 'hover', 'block-overflow'];
+
+  return options.css4 ?
+    mediaQueryTypes.concat(css4MediaQueryTypes) :
+    mediaQueryTypes;
 }
--- test/test.js
+++ test/test.js
@@ -3,7 +3,23 @@ var mediaQueryTypes = require('..');
 
 describe('media-query-types', function() {
 
-  it('should do something awesome', function() {
-    assert.deepEqual(mediaQueryTypes(), ['all', 'braille', 'embossed', 'handheld', 'print', 'projection', 'screen', 'speech', 'tty', 'tv']);
+  it('should return the correct array', function() {
+    assert.deepEqual(
+      mediaQueryTypes(),
+      [
+        'all', 'braille', 'embossed', 'handheld', 'print', 'projection', 'screen',
+        'speech', 'tty', 'tv'
+      ]
+    );
+  });
+
+  it('should include css4 types when css4: true is specified', function() {
+    assert.deepEqual(
+      mediaQueryTypes({ css4: true }),
+      [
+        'all', 'braille', 'embossed', 'handheld', 'print', 'projection', 'screen',
+        'speech', 'tty', 'tv', 'pointer', 'hover', 'block-overflow'
+      ]
+    );
   });
 });
